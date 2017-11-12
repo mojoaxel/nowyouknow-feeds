556bed2a24f278ccd80e3e213a3cf916526a6f38
Alexander Wunschik <alex@wunschik.net>
2017-11-12 18:48:21 +0100

    fixed enclosure size

--- make.js
+++ make.js
@@ -3,11 +3,13 @@ const ora = require('ora');
 const extend = require('extend');
 const moment = require('moment');
 const RSS = require('rss');
+const promisify = require("promisify-node");
+const remoteFileSize = async (url) => promisify(require('remote-file-size'))(url);
 
 const package = require('./package.json');
 const data = require('./nowyouknow.json');
 
-function generateFeed(name, format) {
+async function generateFeed(name, format) {
 	const filename = `${name}_${format}.xml`;
 	let spinner = new ora(`creating feed ${filename}`).start();
 
@@ -17,15 +19,22 @@ function generateFeed(name, format) {
 	});
 
 	var feed = new RSS(settings);
-	data.items.forEach((item, index) => {
+	let index = 0;
+	for (const item of data.items) {
 		let url = data.cdn + item[format];
+		spinner.text = `gettings filesize of ${url}`;
+		let size = await remoteFileSize(url);
 
 		let episode = {
 			title: item.title,
 			description: `${item.title} (${format.toUpperCase()})`,
 			url: url,
-			date: moment(data.feed.pubDate).seconds(index), // any format that js Date can parse.
-			enclosure: {url: url, type: `audio/${format}`},
+			date: moment(data.feed.pubDate).seconds(index++), // any format that js Date can parse.
+			enclosure: {
+				url: url,
+				size: size,
+				type: `audio/${format}`
+			},
 			custom_elements: []
 		};
 
@@ -62,7 +71,8 @@ function generateFeed(name, format) {
 		};
 
 		feed.item(episode);
-	})
+	};
+	spinner.text = `saving xml to file ${filename}`;
 
 	var xml = feed.xml({indent: true});
 	fs.writeFile(filename, xml, (err) => {
@@ -74,5 +84,9 @@ function generateFeed(name, format) {
 	});
 }
 
-generateFeed("all", "mp3");
-generateFeed("all", "ogg");
+async function genAllFeeds(name) {
+	await generateFeed(name, "mp3");
+	await generateFeed(name, "ogg");
+}
+
+genAllFeeds("all");
